91f6db783e6314cc8f9de6a3e92472285c8acf08
wiebe <wipeautcrafter@gmail.com>
2022-06-16 03:19:03 +0200

    fixed mobile bug with vh...

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 import createScene from "./background.js";
 import scrollReveal from "./scrollReveal.js";
 
+const setInnerHeight = () => {
+    document.documentElement.style.setProperty("--vh", `${window.innerHeight}px`);
+};
+
 window.addEventListener("DOMContentLoaded", () => {
     // skip to content button
     const skipEl = document.querySelector(".skip-btn");
@@ -11,7 +15,12 @@ window.addEventListener("DOMContentLoaded", () => {
         });
     });
 
+    // bind inner height
+    window.addEventListener("resize", setInnerHeight);
+    window.addEventListener("orientationchange", setInnerHeight);
+    setInnerHeight();
+
+    // create scene
     createScene(1000 / 60); // 60 fps
     scrollReveal(".section", 400, 100); // reveal all sections
 });
-
